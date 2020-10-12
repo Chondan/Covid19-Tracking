8e21d19a48d951c718d6c2c47287ce26b568ef99
Chondan <chondansusuwan@outlook.com>
2020-10-12 15:16:11 +0700

    first commit

---
+++ precache-manifest.38683bd95cd47591d7c422d8b54b2e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4797b6f9144fd82d3c5ba552606f1570",
+    "url": "/Covid19-Tracking/index.html"
+  },
+  {
+    "revision": "f4858c6af22f453a206e",
+    "url": "/Covid19-Tracking/static/css/main.04043cd2.chunk.css"
+  },
+  {
+    "revision": "e7713050ccb743494609",
+    "url": "/Covid19-Tracking/static/js/2.8296ec61.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid19-Tracking/static/js/2.8296ec61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4858c6af22f453a206e",
+    "url": "/Covid19-Tracking/static/js/main.a20874bc.chunk.js"
+  },
+  {
+    "revision": "1749ce71d1f386eaf305",
+    "url": "/Covid19-Tracking/static/js/runtime-main.1c87e9b9.js"
+  }
+]);
